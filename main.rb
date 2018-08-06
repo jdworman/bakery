f3e4529d493685518cede4a85075821fe13f78ce
Joseph Worman <jdworman84@gmail.com>
2018-08-05 21:38:01 -0400

    added some html

--- main.rb
+++ main.rb
@@ -11,19 +11,27 @@ __END__
 doctype html 
 html
   head 
-    meta charset="utf-8" 
-    title Just Do It 
+    meta charset="Joe Valley Bakery" 
+    title Joe Valley Bakery
     link rel="stylesheet" media="screen, projection" href="/styles.css" 
     /[if lt IE 9] 
       script src="http://html5shiv.googlecode.com/svn/trunk/html5.js" 
   body 
-    h1 Just Do It 
+    h1 Joe Valley Bakery
     == yield 
 
 @@index 
-h2 My Tasks
-ul.tasks
-  li Get Milk
+h2 WELCOME TO JOE VALLEY BAKERY SAN FRANCISCO'S FAVORITE NEIGHBORHOOD BAKERY!
+
+<style>
+body {
+background-color: red;
+}
+h1 {
+  color: yellow;
+}
+</style>
+
  
 
 
