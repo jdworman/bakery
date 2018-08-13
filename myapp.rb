e5f827b84d1c52fdea15f933d264e3242ad8acc8
Joseph Worman <jdworman84@gmail.com>
2018-08-13 19:43:08 -0400

    debugged

--- myapp.rb
+++ myapp.rb
@@ -1,5 +1,7 @@
 require 'sinatra'
-
+require 'curb'
+require 'json'
+ 
 get '/' do
     erb :index
   end
@@ -16,3 +18,12 @@ get '/muffins' do
   erb :muffins
 end
 
+get '/events' do
+  @data = Curl::Easy.perform("https://www.eventbriteapi.com/v3/events/search/?q=baking&sort_by=date&location.address=new+york+city&token=3ZHGTKVD5QVB35RMZTBF")
+  @req = JSON.parse(@data.body_str)
+  erb :events
+end
+
+get '/contact' do
+  erb :contact
+end
